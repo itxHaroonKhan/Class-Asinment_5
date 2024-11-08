96bb565f8400da7738a8a5f8a702378431559459
Code Crafter <162664649+itxHaroonKhan@users.noreply.github.com>
2024-11-07 19:13:51 -0800

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+export default function Home() {
+  return (
+   <div className="h-screen">
+<Navbar/>
+<Hero/>
+   </div>
+  );
+}
